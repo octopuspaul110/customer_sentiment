d00ca82076bd3701634933fcbc91f8da2125ebd3
Adegbite Paul <105382756+octopuspaul110@users.noreply.github.com>
2025-03-01 00:43:35 +0100

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,9 @@ app = FastAPI()
 
 #MODEL_PATH = '/models/best_model.pkl'
 BASE_DIR = os.getcwd()  # This gets the current working directory
-MODEL_PATH = os.path.join(BASE_DIR, "models", "best_model.pkl")  # Update if using .joblib
+#MODEL_PATH = os.path.join(BASE_DIR, "models", "best_model.pkl")  # Update if using .joblib
+
+MODEL_PATH = os.path.join(BASE_DIR, "best_model.pkl")  # Update if using .joblib
 
 #@app.route("/", methods=["GET"])
 #def home():
